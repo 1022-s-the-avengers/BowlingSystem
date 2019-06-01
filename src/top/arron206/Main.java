e76fd1669743af976fc90ef3859e3f842e0aaca0
yiweiting <2638088319@qq.com>
2019-06-01 21:56:05 +0800

    图形界面创建

--- src/top/arron206/Main.java
+++ src/top/arron206/Main.java
@@ -1,8 +1,21 @@
 package top.arron206;
 
-public class Main {
+import javafx.application.Application;
 
+public class Main extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String[] args) {
 	// write your code here
+        Application.launch(args);
     }
 }
