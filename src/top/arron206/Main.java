9ae1d6bb9319aec1f7a0a79d4647cbd252a6d9c2
yiweiting <2638088319@qq.com>
2019-06-04 20:12:56 +0800

    更新readme

--- src/top/arron206/Main.java
+++ src/top/arron206/Main.java
@@ -1,10 +1,14 @@
 package top.arron206;
 
+import top.arron206.model.Member;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends Application{
 
