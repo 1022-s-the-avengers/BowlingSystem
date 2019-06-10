3c8f53738a16563398f49851be9da9da3ae3d82a
Great326 <1502658145@qq.com>
2019-06-10 14:00:47 +0800

    8.0

--- src/top/arron206/controller/ScoreSimulation/CompetitionSimulation.java
+++ src/top/arron206/controller/ScoreSimulation/CompetitionSimulation.java
@@ -1,8 +1,7 @@
 package top.arron206.controller.ScoreSimulation;
 
 import top.arron206.model.Member;
-
-import java.util.ArrayList;
+import java.util.*;
 
 public class CompetitionSimulation {
     private static ArrayList<Member> memberList = new ArrayList<>();
@@ -27,6 +26,34 @@ public class CompetitionSimulation {
         }
     }
 
+    private void classicCompetition(int [] classicPlayers) {
+        int credits[] = new int[16];
+        int totalScore1, totalScore2;
+        for (int num1 = 0; num1 < 16; ++num1) {
+            for (int num2 = 0; num2 < 16; ++num2) {
+                if (num1 == num2)
+                    continue;
+                perRoundSimulator.generate("精英赛", classicPlayers[num1], 1);
+                totalScore1 = perRoundSimulator.getTotalScore();
+                perRoundSimulator.generate("精英赛", classicPlayers[num2], 1);
+                totalScore2 = perRoundSimulator.getTotalScore();
+                if (totalScore1 > totalScore2)
+                    credits[num1]++;
+                else
+                    credits[num2]++;
+            }
+        }
+        insertCredit(classicPlayers, credits);
+    }
+
+    private void insertCredit (int [] classicPlayers, int[] credits) {
+        Member member;
+        for (int i = 0; i < 15; ++i) {
+            member = memberList.get(memberList.indexOf(new Member(classicPlayers[i])));
+            member.setCredit(credits[i]);
+            member.insertMember();
+        }
+    }
     private void initializeMemberInfo() {
         for (int i = 1; i <= 60; ++i) {
             BasicInfoGenerator basicInfoGenerator = new BasicInfoGenerator();
